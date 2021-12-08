6b835960998e275050e38e2cbc0a86dd03ae93f3
Sev <90162122+sev2021@users.noreply.github.com>
2021-12-08 18:06:31 +0000

    Review

---
+++ Java/Codea/MyProfile.java
@@ -0,0 +1,9 @@
+ublic class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Code";
+    int age = 33;
+    double desiredSalary = 2_000_000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}

59c44ddc3737ec2420a652c83c8319b871a6628d
Sev <90162122+sev2021@users.noreply.github.com>
2022-07-04 18:26:21 +0100

    Java comment added

--- JS/Node/test-solution.py
+++ JS/Node/test-solution.py
@@ -13,3 +13,26 @@ for line in sys.stdin:
         B = int(arr[1])
         C = int(arr[2])
         print(*({0:i, i%A: "F", i%B: "B", i%(A*B): "FB"}[0] for i in range(1, C+1)))
+
+# Java solution:       
+#      
+# import java.util.*;
+# public class Test{
+# 	public static void main(String[] args) {
+# 		String inp = new Scanner(System.in).nextLine();
+# 		String[] inpArray = inp.split(" ");
+# 		String outStr = "";
+# 		if(inpArray.length > 2) {
+# 			int a = Integer.parseInt(inpArray[0]);
+# 			int b = Integer.parseInt(inpArray[1]);
+# 			int c = Integer.parseInt(inpArray[2]);
+# 			for(int i=1; i<=c; i++) {
+# 				if(i%a == 0) outStr += "F";
+# 				if(i%b == 0) outStr += "B";
+# 				if(i%a > 0 && i%b > 0) outStr += i;
+# 				outStr += " ";
+# 			}
+# 			System.out.println("\n" + outStr);
+# 		}
+# 	}
+# }

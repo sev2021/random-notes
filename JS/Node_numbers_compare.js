2abcd1c5666ed64b3c3fa560355f6e87680e4483
Sev <90162122+sev2021@users.noreply.github.com>
2022-07-17 08:37:45 +0100

    snipped for record

---
+++ JS/Node_numbers_compare.js
@@ -0,0 +1,20 @@
+var inpArr = [];
+var arrInd = 0;
+var topArr = 0;
+
+console.log("Enter number " + arrInd);
+process.stdin.on('data',  data => {
+	if(+data.toString() == 0) { 
+		console.log("\nInput end.");
+		process.stdin.destroy() 
+	}
+	
+	else {
+		inpArr[arrInd] = +data.toString();
+		if(inpArr[arrInd] > inpArr[arrInd - 1]) topArr = arrInd;	
+		console.log("Highest number is: " + inpArr[topArr]);
+		arrInd ++;
+	
+		console.log("\nEnter number " + arrInd);
+	}
+});
